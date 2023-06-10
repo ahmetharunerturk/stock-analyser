c4f0ab2faffdb4886d546b713bd7faef6bc80c04
Ahmet Ertürk <is_ahmet@outlook.com>
2023-06-10 20:56:42 +0200

    Project second work

--- stock_analyser/webapp/urls.py
+++ stock_analyser/webapp/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/',views.BASE, name='BASE' ),
+    path('dashboard/',views.BASE, name='dashboard' ),
     path('',views.Home, name='Home' ),
+    path('features/',views.Features, name='Features' ),
+    path('pricing/',views.Pricing, name='Pricing' ),
+    path('faqs/',views.FAQs, name='FAQs' ),
+    path('about/',views.About, name='About' ),
+    path('contact/',views.Contact, name='Contact' ),
 ]
 
--- stock_analyser/webapp/views.py
+++ stock_analyser/webapp/views.py
@@ -5,3 +5,18 @@ def BASE(request):
 
 def Home(request):
     return render(request, 'index.html')
+
+def Features(request):
+    return render(request, 'features.html')
+
+def Pricing(request):
+    return render(request, 'pricing.html')
+
+def FAQs(request):
+    return render(request, 'faqs.html')
+
+def About(request):
+    return render(request, 'about.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
